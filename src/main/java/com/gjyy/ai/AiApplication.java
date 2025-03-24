46f88d244f3f6ba357433af286b3ac4d69c1557a
buhang1985 <buhang1985@163.com>
2025-03-24 14:06:38 +0800

    yzh

--- src/main/java/com/gjyy/ai/AiApplication.java
+++ src/main/java/com/gjyy/ai/AiApplication.java
@@ -2,6 +2,7 @@ package com.gjyy.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.client.RestTemplate;
@@ -10,14 +11,20 @@ import org.springframework.web.client.RestTemplate;
 public class AiApplication {
 
     public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(AiApplication.class, args);
+        System.out.println("DataSource URL: " + context.getEnvironment().getProperty("spring.datasource.url"));
+        System.out.println("DataSource Username: " + context.getEnvironment().getProperty("spring.datasource.username"));
+        System.out.println("DataSource Driver: " + context.getEnvironment().getProperty("spring.datasource.driver-class-name"));
         SpringApplication.run(AiApplication.class, args);
+
+
     }
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate();
-    }
+//    @Bean
+//    public JdbcTemplate jdbcTemplate() {
+//        return new JdbcTemplate();
+//    }
 }
